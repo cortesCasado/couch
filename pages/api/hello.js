b9dabd71494d6abd4ec59b6c5faffa78f24d5b84
Alvaro Cortes <alvarocortescasado@gmail.com>
2022-04-06 17:13:38 +0200

    Initial commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  const { name } = req.query;
+
+  const dbs = await fetch(`http://localhost:5984/_all_dbs`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Basic ${Buffer.from(`admin:password`).toString('base64')}`,
+    },
+  }).then(r => r.json());
+
+
+  res.status(200).json({ name: JSON.stringify(dbs) })
 }
