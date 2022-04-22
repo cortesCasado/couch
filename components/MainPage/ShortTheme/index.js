e9bd5a05db4a0cbbf441ea28adc723c73f716cd5
Francisco-Borrego <fraborcar@alum.us.es>
2022-04-22 12:15:51 +0200

    Portada con posts recientes y temás más populares

---
+++ components/MainPage/ShortTheme/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function index({title, numberPosts}) {
+  return (
+    <div className='border-2 border-black'>
+      <p>{title}: {numberPosts} posts</p>
+    </div>
+  )
+}
+
+export default index
